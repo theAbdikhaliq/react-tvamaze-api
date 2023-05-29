9e6e19ac330ae6c8b172af8e542a3515853c8d81
AhmetMohamet <ahmetsaeed128@gmail.com>
2023-05-29 16:53:14 +0300

    update style

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Header from "./Components/Header";
 import Search from "./Components/Search";
 import MovieCard from "./Components/MovieCard";
 function App() {
-  return (
-    <div classNameName="App">
-      <Header />
-      <div className="main">
-        <Search />
-        <div className="movies-section">
-          {/* <!-- one card --> */}
-          <MovieCard />
-          {/* <!-- one card --> */}
-        </div>
-      </div>
+ return (
+  <div classNameName="App">
+   <Header />
+   <div className="main">
+    <Search />
+    <div className="movies-section">
+     {/* <!-- one card --> */}
+
+     {/* <!-- one card --> */}
     </div>
-  );
+   </div>
+  </div>
+ );
 }
 
 export default App;
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,16 +1,22 @@
+import "../index.css";
 function MovieCard() {
-  return (
-    <div className="movie-card">
-      <div className="movie-image">
-        <img
-          src="https://i.pinimg.com/originals/a2/34/75/a234753b69e3bd27b7f1d448956c38af.jpg"
-          alt="movie"
-        />
-      </div>
+ return (
+  <div className="movie-card">
+   <div className="movie-image">
+    <img
+     src="https://i.pinimg.com/originals/a2/34/75/a234753b69e3bd27b7f1d448956c38af.jpg"
+     alt="movie"
+    />
+   </div>
 
-      <h3 className="movie-heading">Breaking Bad</h3>
-    </div>
-  );
+   <h3 className="movie-heading">Breaking Bad</h3>
+   <ul className="list">
+    <li>Drama</li>
+    <li>Romace</li>
+   </ul>
+   <button>Watch Now</button>
+  </div>
+ );
 }
 
 export default MovieCard;
