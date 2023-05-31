9681aa102f937989fb2f5b6843435d3535efaa4d
Abdikhaliq Mohamoud <mo.abdikhaliq@gmail.com>
2023-05-31 16:29:16 +0300

    App.js configured successfully...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import Header from "./Components/Header";
 import Search from "./Components/Search";
 import MovieCard from "./Components/MovieCard";
+import { useState } from "react";
+import axios from "axios";
+
+
 function App() {
- return (
-  <div classNameName="App">
-   <Header />
-   <div className="main">
-    <Search />
-    <div className="movies-section">
-     {/* <!-- one card --> */}
-     <MovieCard />
-     {/* <!-- one card --> */}
-    </div>
-   </div>
+    
+    const [movies, setMovies] = useState([])
+    const [inputVal, setInputVal] = useState()
+
+    const fetchOnClick = () => {
+      axios.get(`https://api.tvmaze.com/search/shows?q=${inputVal}`)
+           .then((res) => {
+            setMovies(res.data)
+            console.log(movies)
+           });
+    }
+
+    const handleOnChange = (e) => {
+        setInputVal(e.target.value)
+    }
+
+
+    return (
+    <div className="App">
+        <Header />
+        <div className="main">
+            <Search fetchOnClick={fetchOnClick} handleOnChange={handleOnChange}/>
+            <div className="movies-section">
+                {/* <!-- one card --> */}
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} />
+                ))}
+            </div>
+        </div>
   </div>
  );
 }
